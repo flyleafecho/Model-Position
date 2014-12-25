09704c15bfbffdc1c2ff39de525cab936288c12e
Echo <flyleafecho@gmail.com>
2014-12-25 16:44:59 +0800

    Finish revising, version 1, only for cpLDA
    Signed-off-by: Echo <flyleafecho@gmail.com>

--- src/edu/psu/topic/CitePart_LDA.java
+++ src/edu/psu/topic/CitePart_LDA.java
@@ -1,15 +1,24 @@
 package edu.psu.topic;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.TreeSet;
 
 import edu.psu.types.ContextDocument;
 import edu.psu.types.Corpus;
+import edu.psu.types.IDSorter;
 import edu.psu.types.LabelAlphabet;
 import edu.psu.types.Paper;
 import edu.psu.util.Randoms;
@@ -17,11 +26,10 @@ import gnu.trove.TIntIntHashMap;
 
 public class CitePart_LDA extends Model {
 	public int numParts = 0;
+	public ArrayList<Paper> paperList = new ArrayList<Paper>();
+	public ArrayList<Paper> fullpaperList = new ArrayList<Paper>();
 
-	public Corpus readData(String docDir, String citationDir) throws IOException, FileNotFoundException{
-		ArrayList<Paper> paperList = new ArrayList<Paper>();
-		ArrayList<Paper> fullpaperList = new ArrayList<Paper>();
-		
+	public Corpus readData(String docDir, String citationDir) throws IOException, FileNotFoundException{			
 		File paper_info = new File(docDir);
 		BufferedReader metaReader = new BufferedReader(new InputStreamReader(new FileInputStream(paper_info),"utf-8"));
 		File paper_citation = new File(citationDir);
@@ -100,8 +108,13 @@ public class CitePart_LDA extends Model {
 		//corpus.typeTopicCounts = new TIntIntHashMap[corpus.numTypes];
 		//corpus.tokensPerTopic = new int[numTopics];
 		corpus.citationsPerTopic = new int[numTopics];
+		corpus.citationsPerTopicByPart = new int[numParts][numTopics];
+		
 		this.phi_train = new double[Corpus.citationAlphabet.size()][numTopics];	//topic_citation
 		this.theta_train = new double[Corpus.docAlphabet.size()][numTopics];	//doc_topic
+		
+		this.theta_train_part = new double[numParts][Corpus.docAlphabet.size()][numTopics];
+		
 		this.psi_train = new double[Corpus.docAlphabet.size()][numParts];	//doc_part
 		//citationTopicCounts(大小不加docAlphabet应该也是对的)
 		//Array, per obj is a map which represents specific citation's times are assigned to topics
@@ -117,13 +130,207 @@ public class CitePart_LDA extends Model {
 		this.random = new Randoms();
 		for(int doc_index = 0; doc_index < corpus.docs.size(); doc_index++){
 			ContextDocument doc = corpus.docs.get(doc_index);
+			//doc.citationTopicAssignmentDuplicate = new int[doc.citationSequence.getLength()];
+			doc.citationTAByPart = new int[numParts][];
+			for(int i = 0; i < numParts; i++){
+				doc.citationTAByPart[i] = new int[doc.citationSequenceByPart[i].getLength()];
+			}
+			
+			//Initiate different parts separately
+			for(int i = 0; i < numParts; i++){
+				int[] keys = doc.citationSequenceByPart[i].getFeatures();
+				for(int j = 0; j < doc.citationSequenceByPart[i].getLength(); j++){
+					int topic = r.nextInt(numTopics);
+					doc.citationTAByPart[i][j] = topic;
+					
+					corpus.citationTopicCounts[keys[j]].adjustOrPutValue(topic, 1, 1);
+					corpus.citationsPerTopicByPart[i][topic]++;			//used for estimate doc_topic
+				}
+			}
 		}
 	}
+	
+	public void sampleCorpus(Corpus corpus, int iterations, boolean isContextAware){
+		for(int i = 0; i < iterations; i++){
+			for(int j = 0; j < corpus.docs.size(); j++){
+				sampleOneDocument(corpus,(ContextDocument) corpus.docs.get(j));
+			}
+		}
+	}
+	
+	public void sampleOneDocument(Corpus corpus, ContextDocument doc){
+		
+		TIntIntHashMap[] doc_topic_by_part = new TIntIntHashMap[numParts];  //topic, and the occurrance time of the topic
+		for(int i = 0; i < numParts; i++)
+			doc_topic_by_part[i] = new TIntIntHashMap();
+		
+		for(int i = 0; i < numParts; i++){
+			int[] keys = doc.citationSequenceByPart[i].getFeatures();
+			for (int t = 0; t < doc.citationSequenceByPart[i].getLength(); t++) 
+			{
+				doc_topic_by_part[i].adjustOrPutValue(doc.citationTAByPart[i][t], 1, 1);
+			}
+		}
+
+		for(int i = 0; i < numParts; i++){
+			int[] keys = doc.citationSequenceByPart[i].getFeatures();
+			for(int t = 0; t < keys.length; t++){
+				int topic = doc.citationTAByPart[i][t];
+				
+				corpus.citationsPerTopicByPart[i][topic]--;
+				corpus.citationTopicCounts[keys[t]].adjustOrPutValue(topic, -1, 0);
+				doc_topic_by_part[i].adjustOrPutValue(topic, -1, 0);
+				
+				//TIntIntHashMap currentTypeTopicCounts = corpus.citationTopicCounts[keys[t]];
+				double[] topicDistribution = new double[numTopics];
+				double topicDistributionSum = 0, weight = 0;
+								
+				for(int k = 0; k < numTopics; k++){
+					int topicCitationSum = corpus.citationsPerTopicByPart[0][k] + corpus.citationsPerTopicByPart[1][k];
+					weight = (doc.citationSequenceByPart[i].getLength()+gamma - 1)*(doc_topic_by_part[i].get(k) + alpha[k])*
+								(corpus.citationTopicCounts[keys[t]].get(k)+beta)/(topicCitationSum + betaSum);
+					topicDistributionSum += weight;
+					topicDistribution[k] = weight;
+				}
+				
+				topic = random.nextDiscrete(topicDistribution,topicDistributionSum);
+				doc.citationTAByPart[i][t] = topic;
+				
+				corpus.citationsPerTopicByPart[i][topic]++;
+				corpus.citationTopicCounts[keys[t]].adjustOrPutValue(topic, 1, 1);
+				doc_topic_by_part[i].adjustOrPutValue(topic, 1, 1);
+			}
+		}
+	}
+	
+	public void estimateParameters_single(Corpus corpus){
+		for(int i = 0; i < numParts; i++){
+			for(int d = 0; d < corpus.docs.size(); d++){
+				this.psi_train[d][i] = (corpus.docs.get(d).citationSequenceByPart[i].getLength() + gamma)/
+											(corpus.docs.get(d).citationSequence.getLength() + gammaSum);	//this is the final value of psi
+				for(int k = 0; k < numTopics; k++){
+					this.theta_train_part[i][d][k] = alpha[k]/(corpus.docs.get(d).citationSequenceByPart[i].getLength() + alphaSum);
+				}
+				
+				ContextDocument doc = corpus.docs.get(d);
+				int[] topics = doc.citationTAByPart[i];
+				for(int k : topics){
+					this.theta_train_part[i][d][k] += 1/(corpus.docs.get(d).citationSequenceByPart[i].getLength() + alphaSum); 
+				}
+			}
+		}
+		
+		for(int c = 0; c < corpus.citationAlphabet.size(); c++){
+			for(int k = 0; k < numTopics; k++){
+				int topicCitationSum = corpus.citationsPerTopicByPart[0][k] + corpus.citationsPerTopicByPart[1][k];
+				this.phi_train[c][k] = (corpus.citationTopicCounts[c].get(k) + beta)/(topicCitationSum+betaSum);
+			}
+		}
+	}
+	
+	public void writeResult(String outDir, Corpus corpus) throws IOException{
+		File output = new File(outDir);
+		Formatter out = new Formatter(new StringBuilder(), Locale.US);
+		BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output,false)));
+		
+		bw.write("# Topic_Citation");  
+		bw.newLine();
+		
+		HashMap<Integer,String> citationMap=new HashMap<Integer,String>();
+		ArrayList<TreeSet<IDSorter>> topicSortedCitation = getSortedCitation(corpus,numTopics,citationMap);
+		
+		for(int topic = 0; topic < numTopics; topic++){
+			Iterator<IDSorter> iterator = topicSortedCitation.get(topic).iterator();
+			
+			out = new Formatter(new StringBuilder(), Locale.US);
+			out.format ("%d\n",topic);
+			int rank = 0;
+			while (iterator.hasNext()) {
+				IDSorter idCountPair = iterator.next();
+				if(idCountPair.getWeight()>0.0001){
+					String title=null;
+					int year=-1;
+					for(int i = 0; i < fullpaperList.size(); i++){
+						if(fullpaperList.get(i).getID().equals(citationMap.get(idCountPair.getID()))){
+							title=fullpaperList.get(i).getTitle();
+							year = fullpaperList.get(i).getYear();
+							break;
+						}
+					}
+					out.format("%s\t%.4f\t%s\t%d\n", citationMap.get(idCountPair.getID()), idCountPair.getWeight(),title,year);
+					rank++;
+				}
+			}
+		}
+		
+		bw.write("# Doc_Topic");  
+		bw.newLine();
+		for(int i = 0; i < corpus.docAlphabet.size(); i++){
+			out = new Formatter(new StringBuilder(), Locale.US);
+			String id = (String) corpus.docAlphabet.lookupObject(i);
+			String title=null;
+			int year=-1;
+			for(int z = 0; z < fullpaperList.size(); z++){
+				if(fullpaperList.get(z).getID().equals(id)){
+					title=fullpaperList.get(z).getTitle();
+					year = fullpaperList.get(z).getYear();
+					break;
+				}
+			}
+			out.format("%s\t", corpus.docAlphabet.lookupObject(i));
+			double[] max ={0,0}; int[] assign = {-1,-1};
+			for(int part = 0; part < numParts; part++){
+				for(int topic = 0; topic < numTopics; topic++){
+					if(theta_train_part[part][i][topic]>max[part]){
+						max[part] = theta_train_part[part][i][topic];
+						assign[part]=topic;
+					}
+					out.format ("%.4f\t",theta_train_part[part][i][topic]);
+				}
+			}
+			out.format("%d\t%.4f\t%d\t%.4f\t%s\t%d", assign[0],max[0],assign[1],max[1],title,year);
+			String line = out.toString();
+			System.out.println(line);
+			bw.write(line);
+			bw.newLine();
+		}
+		
+		out.close();
+		bw.close();
+	}
+	
+	public ArrayList<TreeSet<IDSorter>> getSortedCitation (Corpus corpus,int numTopics,HashMap<Integer,String> citationMap) {
+		
+		ArrayList<TreeSet<IDSorter>> topicSortedCitation = new ArrayList<TreeSet<IDSorter>>(numTopics);
+
+		// Initialize the tree sets
+		for (int topic = 0; topic < numTopics; topic++) {
+			topicSortedCitation.add(new TreeSet<IDSorter>());
+		}
+
+		// Collect counts
+		ArrayList<String> entries=Corpus.citationAlphabet.entries;
+		for (int type = 0; type < Corpus.citationAlphabet.size(); type++) {
+			int index=Corpus.citationAlphabet.lookupIndex(entries.get(type));
+			citationMap.put(index, entries.get(type));
+			for (int i = 0; i < numTopics; i++) {
+				topicSortedCitation.get(i).add(new IDSorter(index, phi_train[type][i]));
+			}
+		}
+		return topicSortedCitation;
+	}
+	
+	public double sampleLikelihood(int numSamples, Corpus corus){return 0;}
+	
+	public double sampleLikelihood(int numSamples, Corpus corpus,
+			TIntIntHashMap split){return 0;}
+	
 	public static void main(String[] args) throws Exception{
 		String paper_info = "";
 		String paper_citation = "";
+		String result = "";
 		
-		int iteration = 5000;
+		int iterations = 5000;
 		
 		CitePart_LDA cpLDA = new CitePart_LDA();
 		
@@ -135,5 +342,10 @@ public class CitePart_LDA extends Model {
 		
 		LabelAlphabet labelAlp= null;	//Just for matching the abstract method in Model
 		cpLDA.InitializeAssignments(corpus,labelAlp);
+		
+		cpLDA.sampleCorpus(corpus, iterations, false);
+		cpLDA.estimateParameters_single(corpus);
+		
+		cpLDA.writeResult(result,corpus);
 	}
 }
--- src/edu/psu/topic/Model.java
+++ src/edu/psu/topic/Model.java
@@ -17,7 +17,6 @@ public abstract class Model {
 			LabelAlphabet topicAlphabet);
 
 	public  int numTopics;
-	//public int numSamples = 0;
 	public Random r = new Random();
 	public Randoms random;
 	
@@ -32,6 +31,7 @@ public abstract class Model {
 	
 	//Model Results
 	public double[][] theta_train;	//M*K
+	public double[][][] theta_train_part;	//P*M*K
 	public double[][] phi_train;	//Vocabulary*K
 	public double[][] psi_train;	//M*2
 	
@@ -44,266 +44,16 @@ public abstract class Model {
 	public abstract void sampleCorpus(Corpus corpus, int iterations,
 			boolean isContextAware);
 	
-	public void learnParameters(Corpus corpus){
-		docLengthCounts = new int[corpus.maxTokens+1];
-		topicDocCounts = new int[numTopics][corpus.maxTokens + 1];
-		for (int d = 0; d < corpus.docs.size(); d++) {
-			ContextDocument doc = (ContextDocument) corpus.docs.get(d);
-			
-			docLengthCounts[doc.wordLength]++;
-			int[] topics = doc.topicAssignments.getFeatures();
-			int[] topicCounts=  new int[numTopics];
-			for(int i=0;i<topics.length;i++){
-				topicCounts[topics[i]]++;
-				
-			}
-			for (int topic=0; topic < numTopics; topic++) {
-				
-				topicDocCounts[topic][ topicCounts[topic] ]++;
-				//System.out.print(topicCounts[topic] + "\t");
-			}
-			//System.out.println();
-		}
-		alphaSum = Dirichlet.learnParameters(alpha, topicDocCounts, docLengthCounts);
-	}
-
-	
 	public abstract void estimateParameters_single(Corpus corpus);
 	/**
 	 *  Return an array of sorted sets (one set per topic). Each set 
 	 *   contains IDSorter objects with integer keys into the alphabet.
 	 *   To get direct access to the Strings, use getTopWords().
 	 */
-	public ArrayList<TreeSet<IDSorter>> getSortedWords (Corpus corpus,int numTopics) {
-	
-		ArrayList<TreeSet<IDSorter>> topicSortedWords = new ArrayList<TreeSet<IDSorter>>(numTopics);
 
-		// Initialize the tree sets
-		for (int topic = 0; topic < numTopics; topic++) {
-			topicSortedWords.add(new TreeSet<IDSorter>());
-		}
-
-		// Collect counts
-		for (int type = 0; type < corpus.vocabulary.size(); type++) {
-			for (int i = 0; i < numTopics; i++) {
-				phi_train[type][i] = ((corpus.typeTopicCounts[type].get(i) + beta) / (corpus.tokensPerTopic[i] + betaSum));
-				topicSortedWords.get(i).add(new IDSorter(type, phi_train[type][i]));
-			}
-
-		}
-
-		return topicSortedWords;
-	}
-	
-	// Need overwrited in specific Models
-	public ArrayList<TreeSet<IDSorter>> getSortedCitation (Corpus corpus,int numTopics,HashMap<Integer,String> citationMap) {
-		
-		ArrayList<TreeSet<IDSorter>> topicSortedCitation = new ArrayList<TreeSet<IDSorter>>(numTopics);
-
-		// Initialize the tree sets
-		for (int topic = 0; topic < numTopics; topic++) {
-			topicSortedCitation.add(new TreeSet<IDSorter>());
-		}
-
-		// Collect counts
-		ArrayList<String> entries=Corpus.citationAlphabet.entries;
-		for (int type = 0; type < Corpus.citationAlphabet.size(); type++) {
-			int index=Corpus.citationAlphabet.lookupIndex(entries.get(type));
-			citationMap.put(index, entries.get(type));
-			for (int i = 0; i < numTopics; i++) {			
-				psi_train[type][i] = ((corpus.citationTopicCounts[index].get(i) + gamma) / (corpus.citationsPerTopic[i] + gammaSum));
-				topicSortedCitation.get(i).add(new IDSorter(index, psi_train[type][i]));
-			}
-
-		}
-
-		return topicSortedCitation;
-	}
-	
 	public abstract double sampleLikelihood(int numSamples, Corpus corus);
 
 	public abstract double sampleLikelihood(int numSamples, Corpus corpus,
 			TIntIntHashMap split);
-
-	public double testPerplexity(int numSamples, Corpus corpus) {
-		double ll = 0;
-		int sampleSize = 0, ss = 0;
-
-		for (int doc = 0; doc < corpus.docs.size(); doc++) {
-			ContextDocument document = (ContextDocument) corpus.docs.get(doc);
-			sampleSize += document.wordLength;
-			TIntIntHashMap word_counts = corpus.docs.get(doc).word_counts;
-			int[] keys = word_counts.keys();
-			double tmp_ll = 0;
-			for (int i = 0; i < keys.length; i++) {
-				for (int t = 0; t < numTopics; t++) {
-					tmp_ll += theta_train[doc][t]
-							* phi_train[keys[i]][t];
-
-				}
-				ll += (Math.log(tmp_ll) * word_counts.get(keys[i]));
-				ss += word_counts.get(keys[i]);
-
-			}
-
-		}
-		System.out.println("Samples=" + sampleSize + "docs:"
-				+ corpus.docs.size() + "ss:" + ss);
-
-		// return Math.exp(-1 * ll / sampleSize);
-		return ll;
-	}
-
-	public double modelLogLikelihood(Corpus corpus) {
-		double logLikelihood = 0.0;
-		int nonZeroTopics;
-
-		TIntIntHashMap[] typeTopicCounts = new TIntIntHashMap[corpus.numTypes];
-		int[] tokensPerTopic = new int[numTopics];
-		int[] citationsPerTopic = new int[numTopics];
-
-		TIntIntHashMap[] citationTopicCounts = new TIntIntHashMap[corpus.numCitations];
-		for (int i = 0; i < corpus.citationTopicCounts.length; i++)
-			citationTopicCounts[i] = new TIntIntHashMap();
-		for (int i = 0; i < corpus.typeTopicCounts.length; i++)
-			typeTopicCounts[i] = new TIntIntHashMap();
-
-		for (int doc_index = 0; doc_index < corpus.docs.size(); doc_index++) {
-			ContextDocument doc = (ContextDocument) corpus.docs.get(doc_index);
-
-			int[] topics = doc.topicAssignments.getFeatures();
-			int[] words = doc.wordSequence.getFeatures();
-			// put a check to see if wordlength exceeds words.length
-			for (int i = 0; i < doc.sentenseLength; i++) {
-				for (int j = doc.SentenseBoundaries.get(i).firstElement(); j < doc.SentenseBoundaries
-						.get(i).lastElement(); j++) {
-
-					if (doc.contextObject.contains(i)) {
-
-						typeTopicCounts[words[j]].adjustOrPutValue(topics[j],
-								1, 1);
-						tokensPerTopic[topics[j]]++;
-						// for (int citations = 0; citations < doc.contextObject
-						// .get(i).size(); citations++) {
-
-						// alphabet
-						// topics[j] = r.nextInt(numTopics);
-						// corpus.typeTopicCounts[words[j]].adjustOrPutValue(
-						// topics[j], 1, 1);
-						citationTopicCounts[doc.contextObject.get(i).get(0)]
-								.adjustOrPutValue(topics[j], 1, 1);
-						// corpus.tokensPerTopic[topics[j]]++;
-						citationsPerTopic[topics[j]]++;
-						// }
-					} else {
-
-						typeTopicCounts[words[j]].adjustOrPutValue(topics[j],
-								1, 1);
-						tokensPerTopic[topics[j]]++;
-					}
-				}
-			}
-		}
-
-		// The likelihood of the model is a combination of a
-		// Dirichlet-multinomial for the words in each topic
-		// and a Dirichlet-multinomial for the topics in each
-		// document.
-
-		// The likelihood function of a dirichlet multinomial is
-		// Gamma( sum_i alpha_i ) prod_i Gamma( alpha_i + N_i )
-		// prod_i Gamma( alpha_i ) Gamma( sum_i (alpha_i + N_i) )
-
-		// So the log likelihood is
-		// logGamma ( sum_i alpha_i ) - logGamma ( sum_i (alpha_i + N_i) ) +
-		// sum_i [ logGamma( alpha_i + N_i) - logGamma( alpha_i ) ]
-
-		// Do the documents first
-
-		int[] topicCounts = new int[numTopics];
-		double[] topicLogGammas = new double[numTopics];
-		int[] docTopics;
-		int sampleSize = 0;
-
-		for (int topic = 0; topic < numTopics; topic++) {
-			topicLogGammas[topic] = Dirichlet
-					.logGammaStirling(alpha[topic]);
-		}
-
-		for (int doc = 0; doc < corpus.docs.size(); doc++) {
-			LabelSequence topicSequence = (LabelSequence) corpus.docs.get(doc).topicAssignments;
-
-			docTopics = topicSequence.getFeatures();
-			sampleSize += docTopics.length;
-
-			for (int token = 0; token < docTopics.length; token++) {
-				topicCounts[docTopics[token]]++;
-			}
-
-			for (int topic = 0; topic < numTopics; topic++) {
-				if (topicCounts[topic] > 0) {
-
-					logLikelihood += (Dirichlet
-							.logGammaStirling(alpha[topic]
-									+ topicCounts[topic]) - topicLogGammas[topic]);
-				}
-			}
-
-			// subtract the (count + parameter) sum term
-			logLikelihood -= Dirichlet.logGammaStirling(alphaSum
-					+ docTopics.length);
-
-			Arrays.fill(topicCounts, 0);
-		}
-
-		// add the parameter sum term
-		logLikelihood += corpus.docs.size()
-				* Dirichlet.logGammaStirling(alphaSum);
-
-		// And the topics
-
-		// Count the number of type-topic pairs
-		int nonZeroTypeTopics = 0;
-
-		for (int type = 0; type < corpus.vocabulary.size(); type++) {
-			int[] usedTopics = typeTopicCounts[type].keys();
-
-			for (int topic : usedTopics) {
-				int count = typeTopicCounts[type].get(topic);
-				if (count > 0) {
-					nonZeroTypeTopics++;
-					logLikelihood += Dirichlet.logGammaStirling(beta
-							+ count);
-				}
-			}
-		}
-
-		for (int topic = 0; topic < numTopics; topic++) {
-
-			logLikelihood -= Dirichlet
-					.logGammaStirling((beta * numTopics)
-							+ tokensPerTopic[topic]);
-		}
-
-		logLikelihood += (Dirichlet.logGammaStirling(beta * numTopics))
-				- (Dirichlet.logGammaStirling(beta) * nonZeroTypeTopics);
-
-		//return Math.exp(-1 * logLikelihood / sampleSize);
-		return logLikelihood;
-	}
-
-	public static Model factory(String type) {
-
-		if (type.toLowerCase().equals("citelda")) {
-			return new citeLDA();
-		} else if (type.toLowerCase().equals("linklda")) {
-			return new linkLDA();
-		} else if (type.toLowerCase().equals("citeplsalda")) {
-			return new citePlsaLDA();
-		} else if (type.toLowerCase().equals("linkplsalda")) {
-			return new linkPlsaLDA();
-		} else
-			return null;
-	}
 
 }
